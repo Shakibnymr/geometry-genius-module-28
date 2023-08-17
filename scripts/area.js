f9b81079e89f4e75b0eb40b2035deffc6e26e70e
shakibnymr <shakibnymr11@gmail.com>
2023-08-18 02:22:05 +0600

    function is best

--- scripts/area.js
+++ scripts/area.js
@@ -1,13 +1,42 @@
+function getValue(inputId){
+    const getId = document.getElementById(inputId);
+    const getInnerValue = getId.value;
+    const value = parseFloat(getInnerValue);
+    return value
+}
+function getInnerText(Id){
+    const GetId =document.getElementById(Id);
+    const GetInnerText = GetId.innerText;
+    const innerText = parseFloat(GetInnerText);
+return innerText
+}
+function SetValue(Id, Input){
+const find = document.getElementById(Id);
+find.innerText = Input;
+}
+
 function calculateTriangleArea(){
     // get triangle base value
-    const baseField = document.getElementById('triangle-base');
-    const baseValueText = baseField.value;
-    const base = parseFloat(baseValueText);
-
+    // const baseField = document.getElementById('triangle-base');
+    // const baseValueText = baseField.value;
+    // const base = parseFloat(baseValueText);
+const base = getValue('triangle-base');
     // get triangle height value
-    const heightField = document.getElementById('triangle-height');
-    const heightValueText = heightField.value;
-    const height = parseFloat(heightValueText);
+    // const heightField = document.getElementById('triangle-height');
+    // const heightValueText = heightField.value;
+    // const height = parseFloat(heightValueText);
+    const height = getValue('triangle-height');
 
-    console.log( height)
+    const area = 0.5 * base * height
+    console.log(area);
+    
+    // show area
+   SetValue('triangle-area',area)
+}
+
+function calculateRectangleArea(){
+const width = getValue('rectangle-width');
+const length = getValue('rectangle-length');
+const area = width * length;
+SetValue('rectangle-area',area)
 }
